930219e7a6e2f9589daa35399a26d8d83e976373
Miran <mirankavinda12@gmail.com>
2023-10-17 13:26:54 +0530

    fixed

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ BLUE = (100, 149, 237)
 RED = (188, 39, 50)
 DARK_GREY = (80, 78, 81)
 
+FONT = pygame.font.SysFont("comicsans", 14)
+
 class Planet:
 
     # AU = Astronomical Units 
@@ -20,8 +22,8 @@ class Planet:
     # G = Gravitational constant
     G = 6.67428e-11
 
-    SCALE = 200 / AU # 1AU = 100 Pixels
-    TIMESTEP = 3600 * 24 # 1 Day
+    SCALE = 150 / AU # 1AU = 100 Pixels
+    TIMESTEP = 3600*24 # 1 Day
 
 
     def __init__(self, x, y, radius, color, mass):
@@ -51,32 +53,36 @@ class Planet:
                 y = y * self.SCALE + HEIGHT / 2 
                 updated_points.append((x, y))
 
-            pygame.draw.lines(win, self.color, False, updated_points)
+            pygame.draw.lines(win, self.color, False, updated_points, 2)
 
         pygame.draw.circle(win, self.color, (x, y), self.radius)
 
-    def attration(self, other):
+        if not self.sun:
+             distance_text = FONT.render(f"{self.distance_to_sun/1000, 1}km", 1, WHITE)
+             win.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_height()/2))
+
+    def attraction(self, other):
         other_x, other_y = other.x, other.y
-		distance_x = other_x - self.x
-		distance_y = other_y - self.y
-		distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
 
-		if other.sun:
-			self.distance_to_sun = distance
+        if other.sun:
+            self.distance_to_sun = distance
 
-		force = self.G * self.mass * other.mass / distance**2
-		theta = math.atan2(distance_y, distance_x)
-		force_x = math.cos(theta) * force
-		force_y = math.sin(theta) * force
-		return force_x, force_y
-    
+        force = self.G * self.mass * other.mass / distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+        return force_x, force_y
+ 
     def update_position(self, planets):
         total_fx = total_fy = 0
         for planet in planets:
             if self == planet:
                 continue
 
-            fx, fy = self.attration(planet)
+            fx, fy = self.attraction(planet)
             total_fx += fx
             total_fy += fy
 
@@ -85,47 +91,48 @@ class Planet:
         a = f / m
         """
         self.x_vel += total_fx / self.mass * self.TIMESTEP
-        self.y_vel += total_fy /self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
 
         self.x += self.x_vel * self.TIMESTEP
         self.y += self.y_vel * self.TIMESTEP
         self.orbit.append((self.x, self.y))
 
 def main():
-    run = True
-    clock = pygame.time.Clock()
+	run = True
+	clock = pygame.time.Clock()
 
-    sun= Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
-    sun.sun = True
+	sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+	sun.sun = True
 
-    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
-    earth.y_vel = 29.783 * 1000
+	earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+	earth.y_vel = 29.783 * 1000 
 
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
-    mars.y_vel = 24.077 * 100
+	mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+	mars.y_vel = 24.077 * 1000
 
-    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
-    mercury.y_vel = -47.4 * 1000
+	mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+	mercury.y_vel = -47.4 * 1000
 
-    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 48685 * 10**24)
-    venus.y_vel = -35.02 * 1000
+	venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+	venus.y_vel = -35.02 * 1000
 
-    planets = [sun, earth, mars, mercury, venus]
+	planets = [sun, earth, mars, mercury, venus]
 
-    while run:
-        clock.tick(60)
-        WIN.fill(0, 0, 0)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+	while run:
+		clock.tick(60)
+		WIN.fill((0, 0, 0))
 
-        for planet in planets:
-            planet.update_position(planets)
-            planet.draw(WIN)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
 
-        pygame.display.update()
+		for planet in planets:
+			planet.update_position(planets)
+			planet.draw(WIN)
 
+		pygame.display.update()
 
-    pygame.quit()
+	pygame.quit()
+
 
 main()
