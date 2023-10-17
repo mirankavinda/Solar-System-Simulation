ac0fcb23e69efaac9fe7d7bf331724ec8eebc944
Miran <mirankavinda12@gmail.com>
2023-10-17 11:48:35 +0530

    planets_creating

--- main.py
+++ main.py
@@ -12,14 +12,30 @@ BLUE = (100, 149, 237)
 RED = (188, 39, 50)
 DARK_GREY = (80, 78, 81)
 
+class Planet:
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+
 def main():
     run = True
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
+        #WIN.fill()
+        #pygame.display.update()
 
-        WIN.fill()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
