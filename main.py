d9f137e2229beeaa958a7485f2ffba6f37222fe0
Miran <mirankavinda12@gmail.com>
2023-10-17 11:45:04 +0530

    main_colors

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solor System Simulation")
 
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
+
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -13,6 +19,7 @@ def main():
     while run:
         clock.tick(60)
 
+        WIN.fill()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
